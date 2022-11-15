0471820615cb04881ee51c6f4cb67be895fdd82e
AJIT SHARMA <80311905+ajit-sharmaGH@users.noreply.github.com>
2022-11-15 18:16:25 +0530

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Emoji-interptreter
-Created with CodeSandbox
+
+This is what which can track the expression of emoji if being paste inside the input box.
+
+choose a particular emoji which you want to know its expression.
+
+paste it into the input box, it will disclose the expression hence named as EMOJI ENTERPRETER !
--- src/App.js
+++ src/App.js
@@ -16,54 +16,49 @@ var emojiDictionary = {
   "😁": "Beaming Face with Smiling Eyes",
   "🙃": "Upside-Down Face",
   "😲": "Astonished Face",
-  "🧐": "Face with Monocle"
+  "🧐": "Face with Monocle",
+  "😭": "Extreme Pain",
+  "🫣": "Noisy",
+  "😎": "Bhaukaal",
+  "🙏": "Radhe Radhe (Namaste)",
+  "🥳": "Feeling Khoosh ",
+  "😐": "Suspected trauma",
+  "👋": "Bye, see you soon !",
+  "💪": "Chacha hmre Vidhayak hain "
 };
 
 var emojiWeKnow = Object.keys(emojiDictionary);
-
 export default function App() {
   const [meaning, setMeaning] = useState("");
-
   function emojiInputHandler(event) {
     var userInput = event.target.value;
-
     var meaning = emojiDictionary[userInput];
-
     setMeaning(meaning);
   }
-
   //i will catch you if you will copy my code.😂😂
-  // var [userInput, setUserInput] = useState("");
-
-  // function inputChangehandler(event) {
-  //   //console.log(event.target);
-  //   console.log(event.target.value);
-  //   setUserInput(event.target.value);
-  // }
-
   function emojiClickHandler(emoji) {
     var meaning = emojiDictionary[emoji];
     setMeaning(meaning);
   }
-
   return (
     <div className="App">
-      <h1>❤️Emoji Interpreter❤️</h1>
-
+      <span>❤️Emoji Interpreter❤️</span>
       <input onChange={emojiInputHandler} />
+      <p>{"< < < Emoji Expressions > > >"}</p>
       <h2>{meaning}</h2>
-      <h3>click the following emojies to know there meaning.</h3>
-      {emojiWeKnow.map(function (emoji) {
-        return (
-          <span onClick={() => emojiClickHandler(emoji)} key={emoji}>
-            {emoji}
-          </span>
-        );
-      })}
-
-      <div id="footer">
-        <p> Made with LOVE by AJIT SHARMA. </p>
+      <h3>Click the following emojies to know there meaning.</h3>
+      <div>
+        {emojiWeKnow.map(function (emoji) {
+          return (
+            <span onClick={() => emojiClickHandler(emoji)} key={emoji}>
+              {emoji}
+            </span>
+          );
+        })}
       </div>
+      <p>
+        Made with LOVE ❤️ by <mark>AJIT SHARMA</mark>{" "}
+      </p>
     </div>
   );
 }
